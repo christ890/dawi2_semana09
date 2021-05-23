151c088e39b41575437ee270b09e78644bab06a0
christ890 <christ890.098@gmail.com>
2021-05-23 15:45:18 -0500

    cambio en método

--- src/main/java/com/empresa/controller/AlumnoController.java
+++ src/main/java/com/empresa/controller/AlumnoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,7 +31,7 @@ public class AlumnoController {
 	}
 	
 	@PostMapping
-	public ResponseEntity<Alumno> registra(Alumno alumno){
+	public ResponseEntity<Alumno> registra(@RequestBody Alumno alumno){
 		log.info(">>> Registra <<<<" + alumno.getDni());
 		Alumno registrado = service.insertaActualizaAlumno(alumno);
 		if(registrado != null) {
